418f5422ad1fa58a993bdf7df4c04966ee11f52c
chengi0506 <chengi0506@gmail.com>
2024-11-04 10:08:26 +0800

    202411041008_bk

--- src/components/WelcomePage.js
+++ src/components/WelcomePage.js
@@ -1,15 +1,43 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
 import LanguageSelector from './LanguageSelector';
 import '../styles/WelcomePage.css';
 import { navigateTo } from '../utils/navigation';
+import { API_ENDPOINTS } from '../api/endpoints';
+
 
 function WelcomePage() {
   const [isDragging, setIsDragging] = useState(false);
   const [startY, setStartY] = useState(0);
   const navigate = useNavigate();
   const { t } = useTranslation();
+  const [backgroundStyle, setBackgroundStyle] = useState({});
+  
+    useEffect(() => {
+    fetchStoreInfo();
+  }, []);
+  
+  const fetchStoreInfo = async () => {
+    try {
+      const response = await fetch(API_ENDPOINTS.GET_STORE_INFO);
+      if (!response.ok) throw new Error('Network response was not ok');
+      const data = await response.json();	
+
+      // 如果有背景圖片，設置背景樣式
+      if (data[0]?.backgroundImage) {
+        const imageUrl = `${API_ENDPOINTS.GET_IMAGE}?fileName=${data[0].backgroundImage.replace('/uploads/', '')}`;
+        setBackgroundStyle({
+          backgroundImage: `url(${imageUrl})`,
+          backgroundSize: 'cover',
+          backgroundPosition: 'center',
+          backgroundAttachment: 'fixed',
+        });
+      }
+    } catch (error) {
+      console.error('Error fetching store info:', error);
+    }
+  };
 
   const handleTouchStart = (e) => {
     setIsDragging(true);
@@ -25,7 +53,8 @@ function WelcomePage() {
   };
 
   const handleTouchEnd = () => {
-    setIsDragging(false);
+    setIsDragging(false);													 
+	 
   };
 
 
@@ -36,6 +65,7 @@ function WelcomePage() {
   return (
     <div 
       className="welcome-page"
+	  style={backgroundStyle}
       onTouchStart={handleTouchStart}
       onTouchMove={handleTouchMove}
       onTouchEnd={handleTouchEnd}
